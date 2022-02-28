4d4903bf52a5c74f32f7657aee5d3a2dac7ce886
caraccmv <caraccmv@clarkson.edu>
2022-02-27 20:33:16 -0500

    Clock Gen Seems to be complete

---
+++ Clock_Gen/clock_gen/clock_gen.srcs/sources_1/new/math_real.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 02/27/2022 05:58:38 PM
+-- Design Name: 
+-- Module Name: math_real - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity math_real is
+--  Port ( );
+end math_real;
+
+architecture Behavioral of math_real is
+
+begin
+
+
+end Behavioral;
